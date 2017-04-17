87cc601d886c2c70869c71356b09d56e13d60708
maciozo <maciozo@maciozo.com>
2017-04-17 21:52:26 +0100

    Added more errors
    ERR_INVALID_ADDRESS
    ERR_ILLEGAL_MEMORY_COPY
    ERR_COPY_TO_INVALID
    ERR_READ_FROM_INVALID

--- errors.c
+++ errors.c
@@ -7,19 +7,35 @@ void printError(err2204_t *error)
     switch(error->errno)
     {
         case SUCCESS:
-            fprintf(stderr, "Instruction at %x. The operation completed successfully.\n", error->address);
+            fprintf(stderr, "Instruction at %x. The operation completed successfully. (SUCCESS)\n", error->address);
             break;
 
         case ERR_ADDRESS_OUT_OF_RANGE:
-            fprintf(stderr, "Instruction at %x. The specified destination address is out of range.\n", error->address);
+            fprintf(stderr, "Instruction at %x. The specified destination address is out of range. (ERR_ADDRESS_OUT_OF_RANGE)\n", error->address);
             break;
             
         case ERR_END_ADDRESS_OUT_OF_RANGE:
-            fprintf(stderr, "Instruction at %x. The specified end address is out of range.\n", error->address);
+            fprintf(stderr, "Instruction at %x. The specified end address is out of range. (ERR_END_ADDRESS_OUT_OF_RANGE)\n", error->address);
             break;
             
         case ERR_RAM_ADDRESS_IN_USE:
-            fprintf(stderr, "Instruction at %x. The specified RAM address is already in use.\n", error->address);
+            fprintf(stderr, "Instruction at %x. The specified RAM address is already in use. (ERR_RAM_ADDRESS_IN_USE)\n", error->address);
+            break;
+            
+        case ERR_INVALID_ADDRESS:
+            fprintf(stderr, "Instruction at %x. The specified address is invalid. (ERR_INVALID_ADDRESS)\n", error->address);
+            break;
+            
+        case ERR_ILLEGAL_MEMORY_COPY:
+            fprintf(stderr, "Instruction at %x. Cannot copy from the specified source address to the specified destination address. Most likely because the program attempted to copy data without going through the CPU registers. (ERR_ILLEGAL_MEMORY_COPY)\n", error->address);
+            break;
+            
+        case ERR_COPY_TO_INVALID:
+            fprintf(stderr, "Instruction at %x. The program tried to copy data to an invalid address. (ERR_COPY_TO_INVALID)\n", error->address);
+            break;
+            
+        case ERR_READ_FROM_INVALID:
+            fprintf(stderr, "Instruction at %x. The program tried to read data from an invalid address. (ERR_READ_FROM_INVALID)\n", error->address);
             break;
             
         default:
--- errors.h
+++ errors.h
@@ -18,6 +18,22 @@
 /* The specified address or range of addresses is already in use */
 #define ERR_RAM_ADDRESS_IN_USE -3
 
+/* The specified address is invalid. Most likely no device owns that address */
+#define ERR_INVALID_ADDRESS -4
+
+/* 
+    Cannot copy from the specified source address to the specified destination address.
+    Most likely because the program attempted to copy data without going through the CPU registers.
+*/
+#define ERR_ILLEGAL_MEMORY_COPY -5
+
+/* The program tried to copy data to an invalid address */
+#define ERR_COPY_TO_INVALID -6
+
+/* The program tried to read data from an invalid address */
+#define ERR_READ_FROM_INVALID -7
+
+
 typedef struct
 {
     int errno;
