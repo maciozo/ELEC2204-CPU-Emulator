24395eee2778478e38fa493ce166491841fdcb81
maciozo <maciozo@maciozo.com>
2017-04-19 17:16:37 +0100

    Converted error strings to macros

--- errors.c
+++ errors.c
@@ -8,43 +8,43 @@ void printError(err2204_t *error)
     switch(error->errnum)
     {
         case SUCCESS:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The operation completed successfully. (SUCCESS)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " SUCCESS_STR "\n", error->address);
             break;
 
         case ERR_ADDRESS_OUT_OF_RANGE:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The specified destination address is out of range. (ERR_ADDRESS_OUT_OF_RANGE)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_ADDRESS_OUT_OF_RANGE_STR "\n", error->address);
             break;
 
         case ERR_END_ADDRESS_OUT_OF_RANGE:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The specified end address is out of range. (ERR_END_ADDRESS_OUT_OF_RANGE)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_END_ADDRESS_OUT_OF_RANGE_STR "\n", error->address);
             break;
 
         case ERR_RAM_ADDRESS_IN_USE:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The specified RAM address is already in use. (ERR_RAM_ADDRESS_IN_USE)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_RAM_ADDRESS_IN_USE_STR "\n", error->address);
             break;
 
         case ERR_INVALID_ADDRESS:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The specified address is invalid. (ERR_INVALID_ADDRESS)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_INVALID_ADDRESS_STR "\n", error->address);
             break;
 
         case ERR_ILLEGAL_MEMORY_COPY:
-            fprintf(stderr, "Instruction at %" PRIx64 ". Cannot copy from the specified source address to the specified destination address. Most likely because the program attempted to copy data without going through the CPU registers. (ERR_ILLEGAL_MEMORY_COPY)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_ILLEGAL_MEMORY_COPY_STR "\n", error->address);
             break;
 
         case ERR_COPY_TO_INVALID:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The program tried to copy data to an invalid address. (ERR_COPY_TO_INVALID)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_COPY_TO_INVALID_STR "\n", error->address);
             break;
 
         case ERR_READ_FROM_INVALID:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The program tried to read data from an invalid address. (ERR_READ_FROM_INVALID)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_READ_FROM_INVALID_STR "\n", error->address);
             break;
 
         case ERR_UNEXPECTED_RESULT:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The program received an unexpected result from a function. (ERR_UNEXPECTED_RESULT)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_UNEXPECTED_RESULT_STR "\n", error->address);
             break;
             
         case ERR_UNKNOWN_INSTRUCTION:
-            fprintf(stderr, "Instruction at %" PRIx64 ". The program received an undefined instruction. (ERR_UNKNOWN_INSTRUCTION)\n", error->address);
+            fprintf(stderr, "Instruction at %" PRIx64 ". " ERR_UNKNOWN_INSTRUCTION_STR "\n", error->address);
             break;
 
         default:
--- errors.h
+++ errors.h
@@ -5,39 +5,52 @@
 
 /* The operation completed successfully */
 #define SUCCESS 0
+#define SUCCESS_STR "The operation completed successfully. (SUCCESS)"
 
 /* The target address is outside of the boundaries of the device's memory addresses */
 #define ERR_ADDRESS_OUT_OF_RANGE -1
+#define ERR_ADDRESS_OUT_OF_RANGE_STR "The specified destination address is out of range. (ERR_ADDRESS_OUT_OF_RANGE)"
 
 /*
     The end address is outside of the boundaries of the device's memory addresses.
     Length is probably too long.
 */
 #define ERR_END_ADDRESS_OUT_OF_RANGE -2
+#define ERR_END_ADDRESS_OUT_OF_RANGE_STR "The specified end address is out of range. (ERR_END_ADDRESS_OUT_OF_RANGE)"
 
 /* The specified address or range of addresses is already in use */
 #define ERR_RAM_ADDRESS_IN_USE -3
+#define ERR_RAM_ADDRESS_IN_USE_STR "The specified RAM address is already in use. (ERR_RAM_ADDRESS_IN_USE)"
 
 /* The specified address is invalid. Most likely no device owns that address */
 #define ERR_INVALID_ADDRESS -4
+#define ERR_INVALID_ADDRESS_STR "The specified address is invalid. (ERR_INVALID_ADDRESS)"
 
 /*
     Cannot copy from the specified source address to the specified destination address.
     Most likely because the program attempted to copy data without going through the CPU registers.
 */
 #define ERR_ILLEGAL_MEMORY_COPY -5
+#define ERR_ILLEGAL_MEMORY_COPY_STR "Cannot copy from the specified source address to the specified destination address. Most likely because the program attempted to copy data without going through the CPU registers. (ERR_ILLEGAL_MEMORY_COPY)"
 
 /* The program tried to copy data to an invalid address */
 #define ERR_COPY_TO_INVALID -6
+#define ERR_COPY_TO_INVALID_STR "The program tried to copy data to an invalid address. (ERR_COPY_TO_INVALID)"
 
 /* The program tried to read data from an invalid address */
 #define ERR_READ_FROM_INVALID -7
+#define ERR_READ_FROM_INVALID_STR "The program tried to read data from an invalid address. (ERR_READ_FROM_INVALID)"
 
 /* The program received an unexpected result from a function */
 #define ERR_UNEXPECTED_RESULT -8
+#define ERR_UNEXPECTED_RESULT_STR "The program received an unexpected result from a function. (ERR_UNEXPECTED_RESULT)"
 
 /* The specified instruction has not been defined */
 #define ERR_UNKNOWN_INSTRUCTION -9
+#define ERR_UNKNOWN_INSTRUCTION_STR "The program received an undefined instruction. (ERR_UNKNOWN_INSTRUCTION)"
+
+/* Program exited the main CPU loop for some reason */
+#define ERR_CPU_BREAK -10
 
 
 typedef struct
